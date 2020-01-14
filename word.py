38a66f0b2ba60e65b35134dd67e4b118f7c74b22
Max Carey <maxcarey@protonmail.com>
2020-01-14 09:35:03 -0600

    initial commit

---
+++ word.py
@@ -0,0 +1,71 @@
+import os
+import requests
+import json
+
+apiKey = os.getenv('wordKey')
+print(apiKey)
+
+
+class Word:  
+	   
+	def __init__(self, headword):  
+		self.headword = headword  
+	
+		# Craete an empty list attribute that will store the api information
+		self.apiInfo = []
+
+
+	def get_word_info(self):
+
+
+		url = "https://wordsapiv1.p.rapidapi.com/words/" + self.headword
+		print(url)
+
+		headers = {
+			'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+			'x-rapidapi-key': apiKey
+		}
+
+		response = requests.request("GET", url, headers=headers)
+
+		responseText = response.text
+		responseJSON = json.loads(responseText)
+
+		self.apiInfo.append(responseJSON)
+
+
+
+	def get_definitions_and_examples(self):
+
+		for entry in self.apiInfo[0]['results']:
+			print(entry["definition"])
+			
+			try:
+				print(entry["examples"])
+			except:
+				print("no examples")
+
+	def get_syllables(self):
+
+		#print(self.apiInfo[0]['syllables']['count'])
+		return self.apiInfo[0]['syllables']['count']
+
+
+print("Welcome to the word game")
+print("Please enter your favorite word")
+
+userWord = input()
+gameWord = Word(userWord)
+gameWord.get_word_info()
+gameWord.get_definitions_and_examples()
+
+syllables = gameWord.get_syllables()
+
+print("How many syllables does the word have?")
+
+userSyllalesGuess = input()
+
+if int(userSyllalesGuess) == int(syllables):
+	print("Yes you win")
+else:
+	print("No the word has {}".format(syllables))
